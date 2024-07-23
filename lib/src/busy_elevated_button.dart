2d420593a596ba702d8a925ab05d4d6f49e8bc5d
Samfan <samfan@live.com>
2024-07-23 23:32:02 +0530

    feat: Add BusyElevatedButton and BusyIconButton widgets
    - Create the `BusyElevatedButton` and `BusyIconButton` classes in separate files.
    - Implement the `BusyElevatedButton` and `BusyIconButton` widgets for elevated and icon buttons.

---
+++ lib/src/busy_elevated_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BusyElevatedButton extends StatelessWidget {
+  const BusyElevatedButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/src/busy_icon_button.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BusyIconButton extends StatelessWidget {
+  const BusyIconButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
