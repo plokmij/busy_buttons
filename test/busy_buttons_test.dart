c8f668637dd5f9205489e6283aed65d34ee4ddab
Samfan <samfan@live.com>
2024-07-23 23:43:42 +0530

    feat: add test

--- test/busy_buttons_test.dart
+++ test/busy_buttons_test.dart
@@ -2,11 +2,62 @@ import 'package:flutter_test/flutter_test.dart';
 
 import 'package:busy_buttons/busy_buttons.dart';
 
+import 'package:flutter/material.dart';
+
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  testWidgets('BusyTextButton displays text when not busy',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BusyTextButton(
+            onPressed: () async {},
+            child: const Text('Button'),
+          ),
+        ),
+      ),
+    );
+
+    expect(find.text('Button'), findsOneWidget);
+  });
+
+  testWidgets('BusyTextButton displays text Loading when busy',
+      (WidgetTester tester) async {
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BusyTextButton(
+            onPressed: () async {
+              await Future.delayed(const Duration(seconds: 10));
+            },
+            busyChild: const Text('Loading'),
+            child: const Text('Button'),
+          ),
+        ),
+      ),
+    );
+    await tester.tap(find.text('Button'));
+    expect(find.text('Loading'), findsOneWidget);
+  });
+
+  testWidgets('BusyTextButton calls onPressed when tapped',
+      (WidgetTester tester) async {
+    bool onPressedCalled = false;
+
+    await tester.pumpWidget(
+      MaterialApp(
+        home: Scaffold(
+          body: BusyTextButton(
+            onPressed: () async {
+              onPressedCalled = true;
+            },
+            child: const Text('Button'),
+          ),
+        ),
+      ),
+    );
+
+    await tester.tap(find.text('Button'));
+    expect(onPressedCalled, isTrue);
   });
 }
