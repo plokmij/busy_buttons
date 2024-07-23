aec5b76c4958b677ed2f437481ab123bee9fab2f
Samfan <samfan@live.com>
2024-07-24 00:02:54 +0530

    feat: Introduce lazy loading for images in MainApp widget
    - Add lazy loading for images in the `MainApp` widget to improve page load performance.
    - Update the `onPressed` callback of `BusyTextButton` to remove the `print` statement.
    - Implement the `BusyOutlinedButton` widget for outlined buttons.
    - Create the `BusyTextButton` and `BusyOutlinedButton` classes in separate files.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -5,21 +5,39 @@ void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  int _onPressedCount = 0;
+
+  @override
   Widget build(BuildContext context) {
-    return  MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: BusyTextButton(
-            onPressed: () async{
-             await Future.delayed(const Duration(seconds: 2));
-            },
-            busyChild: const Text('Please Wait'),
-            child: const Text('Click Me'),
-          ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            FittedBox(
+              child: Text('onPressed count: $_onPressedCount'),
+            ),
+            Center(
+              child: BusyTextButton(
+                onPressed: () async {
+                  setState(() {
+                    _onPressedCount++;
+                  });
+                  await Future.delayed(const Duration(seconds: 2));
+                },
+                busyChild: const Text('Please Wait'),
+                child: const Text('Click Me'),
+              ),
+            ),
+          ],
         ),
       ),
     );
